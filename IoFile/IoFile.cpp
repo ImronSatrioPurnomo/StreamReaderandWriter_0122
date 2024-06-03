85ae2bb8748a5c501b7339384a4d824b7a6d0a8d
ImronSatrioPurnomo <satriogames2223@gmail.com>
2024-06-03 09:50:34 +0700

    add program main

---
+++ IoFile/IoFile.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+	ofstream outfile;
+	outfile.open("Saepul.txt");
+	cout << ">=Menulis file, \'q\ untuk keluar" << endl;
+	while (true)
+	{
+		cout << "- ";
+		getline(cin, baris);
+		if (baris == "q") break;
+		outfile << baris << endl;
+	}
+
+	outfile.close();
+}
